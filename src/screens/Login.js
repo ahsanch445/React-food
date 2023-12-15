174e4f0fb2fde5f31db14f74b2ef81b32cec2f8a
Ahsan Ch <ach374392@gmail.com>
2023-12-15 10:36:39 -0800

    first commit

---
+++ src/screens/Login.js
@@ -0,0 +1,67 @@
+
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom'
+import axios from 'axios';
+import "./login.css"
+
+
+
+const Login = () => {
+
+  let navigate = useNavigate()
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://food-api-theta.vercel.app/login', formData);
+
+      if (response.status === 200) {
+        console.log('Registration successful');
+        console.log('User data:', response.data);
+        localStorage.setItem('user', JSON.stringify(response.data));
+        navigate("/")
+        // useEffect for client-side operations
+      
+
+      } else {
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <>
+      <div className="main">
+        <div className="wrapper">
+          <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div className="input">
+              <input type="username" name='username' value={formData.username} onChange={handleChange} placeholder="Email" />
+            </div>
+            <div className="input">
+              <input type="password" name='password' value={formData.password} onChange={handleChange} placeholder="Password"/>
+            </div>
+            <button type="submit" className="btn1">Login</button>
+            <p> Create an Account?
+              <Link to="/signup">SignUp</Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login;
---
+++ src/screens/SignUp.js
@@ -0,0 +1,92 @@
+
+
+import { useState } from 'react';
+
+import axios from 'axios';
+import "./sign.css"
+import { useNavigate, Link } from 'react-router-dom'
+
+
+const Register = () => {
+  let navigate = useNavigate()
+
+  // Initialize state to manage form data
+  const [formData, setFormData] = useState({
+    fullname: '',
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  // Access Next.js router
+  
+
+  // Function to handle form input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send registration data to your server using axios
+      const response = await axios.post('https://food-api-theta.vercel.app/register', formData);
+
+    
+      if (response.status === 200 && response.data && response.data.message === 'Registration successful') {
+        console.log('Registration successful');
+      
+       
+        console.log(localStorage.getItem("user"))
+        navigate("/login")
+
+
+        const username = formData.username;
+        localStorage.setItem('username', username  );
+
+
+      } else {
+        console.error('Registration failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    
+    <>
+
+    <div className="main">
+        <div className="wrapper">
+        <form onClick={handleSubmit}>
+            <h1>Login</h1>
+           
+            <div className="input">
+                <input type="text" name='fullname' value={formData.fullname} onChange={handleChange} placeholder="Fullname" />
+            </div>
+            <div className="input">
+                <input type="text" name='username' value={formData.username} onChange={handleChange} placeholder="Username" />
+            </div>
+            <div className="input">
+                <input type="text" name='email' value={formData.email} onChange={handleChange} placeholder="Email" />
+            </div>
+           
+            <div className="input">
+                <input type="password" name='password' value={formData.password} onChange={handleChange} placeholder="Password"/>
+            </div>
+        <button type="submit" className="btn1">  Login</button>
+        <p> Already have a account?
+        <Link to="/login">Login</Link>
+        </p>
+        </form>
+    </div>
+    </div>
+ 
+    </>
+  );
+};
+
+export default Register;
