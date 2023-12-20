b91c1f686d94da272e54ce020f4baf1f215416a1
Ahsan Ch <ach374392@gmail.com>
2023-12-20 06:47:30 -0800

    upgrade footer

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([])
   const [search, setSearch] = useState('')
   const loadFoodItems = async () => {
-    let response = await fetch("foodData", {
+    let response = await fetch("https://food-api-theta.vercel.app/foodData", {
     
       method: 'POST',
       headers: {
